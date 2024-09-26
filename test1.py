de749a73564bfcaeeadbbcdb9dbd28acde965a92
Samoyed-ken <11460254@me.mcu.edu.tw>
2024-09-26 14:15:05 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 # 先導入後面會用到的套件
 import requests # 請求工具
 from bs4 import BeautifulSoup #解析工具
-import time # 用來暫停程式
+import time #用來暫停程式
 # 要爬的股票
 stock = ["1101","2330","1102"]
     for i in range(len(stock)): # 迴圈依序爬股價
